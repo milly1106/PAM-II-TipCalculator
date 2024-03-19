3d64fd4fe5b7fdf67b248a35020f498da612d12b
Emilly <millysantoss1106@gmail.com>
2024-03-19 12:47:16 -0300

    Criei um layout

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button15Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void Button30percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void PercentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            PorcentagemLabel.Text = $"{PercentagemSlider.Value.ToString()}%";
+
+        }
+    }
+}
